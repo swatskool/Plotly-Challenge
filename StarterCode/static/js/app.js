8b7ccb108c090e342ee6500e5b1ca5e992d12640
swatskool <75335665+swatskool@users.noreply.github.com>
2021-03-30 19:02:35 -0600

    update

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,3 +1,14 @@
-fetch("./samples.json").then(response => {
-   return response.json();
-})
+console.log('here')
+d3.json('samples.json').then(function(data){
+         var dd_list = data.names
+         var dropdown = d3.select('selDataset')
+
+         dd_list.forEach(element => {
+            dropdown.append('option').text(element).property('value',element)
+          });
+
+      console.log(data);
+ 
+   })
+
+console.log('there')
